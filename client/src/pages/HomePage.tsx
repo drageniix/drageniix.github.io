c80a79d9dd707b70a1a86d390bc0fa66932a2c90
Ashley Barrow <drageniix@gmail.com>
2019-08-17 10:14:13 -0500

    Working tree server

--- client/src/pages/HomePage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const HomePage = (): React.ReactElement => (
-    <div>Oh my god, I loaded.</div>
-);
-
-export default HomePage;
--- client/src/redux/subscribe.ts
+++
@@ -1,7 +0,0 @@
-import openSocket from 'socket.io-client';
-const io = openSocket('https://barrow-dev.herokuapp.com');
-
-export default (store: any) => {
-    console.log('Store connected');
-    io.on('test', (data: any) => console.log(data));
-};
